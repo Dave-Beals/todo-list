d5b4c13a44ac66ead3e7e6d2269520a326ffdcdb
Dave <dbeals85@gmail.com>
2017-06-18 22:53:37 -0700

    part of function to collect and push text input working

--- js/script.js
+++ js/script.js
@@ -1,25 +1,35 @@
+console.log("I'm working!")
+
+// FROM ELOQUENT JAVASCRIPT. MAYBE USEFUL?
+// var form = document.querySelector("form");
+// console.log(form.elements[1].type);
+
+
 var taskArray = []
+
+var task = [];
+var name = [];
+var difficulty = [];
+
 var taskInput = document.getElementById("task");
 var nameInput = document.getElementById("name");
 var difficultyInput = document.getElementById("difficulty");
 
-function tasksList(){
-  var submit = event.target;
-  var container = document.getElementById("taskOverview");
-  if(submit.clicked){
-    taskArray.push(submit.id)
-  }
+var theTask = document.getElementById("container");
+
+function taskList(){
+  task.push(taskInput.value);
+  console.log(task[0]);
+  // name.push(nameInput.value);
+  // difficulty.push(difficultyInput.value);
 }
 
-tasks.onsubmit = function(){
-  drawToPage();
-  var trigger = document.getElementById("trigger");
-  trigger.onclick = updatePage;
-}
-function updatePage() {
-  taskArray.push()
-}
+// function taskList(){
+//   if(document.getElementById("taskDave").checked)
+//     name.push()
+// }
 
+// BELOW IS POSSIBLY RELEVANT FROM CLASS EXAMPLE
 // function tasksList(){
 //   window.onload = function(){
 //   drawToPage();
