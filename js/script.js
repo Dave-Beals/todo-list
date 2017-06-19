f184793e7d2b6dc075f3231f02fecde37330f07b
Dave <dbeals85@gmail.com>
2017-06-19 00:55:38 -0700

    difficulty and innerHTML function still not working

--- js/script.js
+++ js/script.js
@@ -1,40 +1,56 @@
-console.log("I'm working!")
+console.log("script is working!")
 
 // FROM ELOQUENT JAVASCRIPT. MAYBE USEFUL?
 // var form = document.querySelector("form");
 // console.log(form.elements[1].type);
 
 
-var taskArray = []
+var tasks = []
 
-var task = [];
+var taskTitle = [];
 var person = [];
 var difficulty = [];
-var taskNames = document.forms[0];
-
 var taskInput = document.getElementById("task");
+var nameInput = document.forms[0];
+var difficultyInput = document.getElementById("difficulty");
+var diffSelected = difficultyInput.options[difficultyInput.selectedIndex].value;
+
 // var nameInput = document.getElementById("name");
 // var difficultyInput = document.getElementById("difficulty");
 
-var theTask = document.getElementById("container");
-
 function taskList(){
-  task.push(taskInput.value);
-  console.log(task[0]);
+  taskTitle.push(taskInput.value);
+  // console.log(task[0]);
 
-  for(i = 0; i<taskNames.length; i++){
-    if(taskNames[i].checked){
-      person.push(taskNames[i].value);
-      console.log(person[0]);
+  for(i = 0; i<nameInput.length; i++){
+    if(nameInput[i].checked){
+      person.push(nameInput[i].value);
+      // console.log(person[0]);
+
+  difficulty.push(diffSelected);
+  // console.log(difficulty[0]);
+
+  tasks.push(taskTitle[0], person[0], difficulty[0]);
+  console.log(tasks);
+
+  presentTask();
     }
   }
-  // difficulty.push(difficultyInput.value);
 }
 
-// function taskList(){
-//   if(document.getElementById("taskDave").checked)
-//     name.push()
-// }
+function presentTask(){
+var container = document.getElementById("container");
+container.innerHTML = "";
+for(var i = 0; i < tasks.length; i++){
+  var newNode = document.createElement("li");
+  newNode.innerHTML = tasks[i];
+  container.appendChild(newNode);
+  }
+}
+
+
+
+
 
 // BELOW IS POSSIBLY RELEVANT FROM CLASS EXAMPLE
 // function tasksList(){
